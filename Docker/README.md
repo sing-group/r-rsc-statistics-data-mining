2f3bfb8a5d1bb1cdd192323aa475381af4018bcd
Hugo López-Fernández <hlfernandez@uvigo.es>
2019-03-27 11:58:15 +0100

    Adds PCA analysis

--- Docker/README.md
+++ Docker/README.md
@@ -1,1 +1,4 @@
-docker run --rm -it -w "$(pwd)" -v "$(pwd):$(pwd)" singgroup/r-base-maldiquant R
+run in the host machine before starting the container
+xhost +
+
+docker run --rm -it -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix  -w "$(pwd)" -v "$(pwd):$(pwd)" singgroup/r-base-maldiquant R
---
+++ pca.R
@@ -0,0 +1,60 @@
+library(pca3d)
+
+imagesDirectory <- "images/pca/"
+
+dir.create(imagesDirectory, recursive=TRUE, showWarnings=FALSE)
+
+source("load-cancer.R")
+
+#	---------------------------------------------------------------------------
+#
+#	1. Principal Components Analysis
+#
+#	---------------------------------------------------------------------------
+#
+#	1.1 Principal Components Analysis using the replicas matrix (binnedPeaksMatrix)
+#
+#	---------------------------------------------------------------------------
+
+pca <- prcomp(binnedPeaksMatrix, scale=TRUE)
+
+summary(pca)
+
+# reduce dataset taking the first PCs that accumulate the 95% of variance
+pc95 <- min(which(cumsum(pca$sdev^2)/sum(pca$sdev^2) >= 0.95))
+binnedPeaksMatrix.pca.95 <- pca$x[,1:pc95]
+
+sample_conditions <- sapply(rownames(binnedPeaksMatrix), function(x) substr(x,1,1))
+
+png(paste0(imagesDirectory, "pca-replicates.png"), width=640, height=480)
+pca2d(pca, group=sample_conditions, components=c(1,2), legend="bottomright")
+dev.off()
+
+# Uncomment the following line to show the 3D visualiation of the PCA
+# pca3d(pca, group=sample_conditions, components=c(1,2,3), legend="bottomright")
+
+#	---------------------------------------------------------------------------
+#
+#	1.2 Principal Components Analysis using the samples matrix (consensusBinnedPeaksMatrix)
+#
+#	---------------------------------------------------------------------------
+pca <- prcomp(consensusBinnedPeaksMatrix, scale=TRUE)
+summary(pca)
+
+# reduce dataset taking the first PCs that accumulate the 95% of variance
+pc95 <- min(which(cumsum(pca$sdev^2)/sum(pca$sdev^2) >= 0.95))
+binnedPeaksMatrix.pca.95 <- pca$x[,1:pc95]
+
+sample_conditions <- sapply(rownames(consensusBinnedPeaksMatrix), function(x) substr(x,1,1))
+
+png(paste0(imagesDirectory, "pca-samples.png"), width=640, height=480)
+pca2d(pca, group=sample_conditions, components=c(1,2), legend="bottomright")
+dev.off()
+
+#with biplot
+png(paste0(imagesDirectory, "pca-samples-biplot.png"), width=640, height=480)
+pca2d(pca, group=sample_conditions, components=c(1,2), legend="bottomright", biplot=TRUE)
+dev.off()
+
+# Uncomment the following line to show the 3D visualiation of the PCA
+# pca3d(pca, group=sample_conditions, components=c(1,2,3), legend="bottomright")
