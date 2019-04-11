b8607960a6db7d1b0321cc8781173ff91477dd4f
Hugo López-Fernández <hlfernandez@uvigo.es>
2019-04-11 18:02:33 +0200

    Add scripts to load MALDIquant datasets

---
+++ load-maldiquant-cancer-fiedler.R
@@ -0,0 +1,35 @@
+library("MALDIquant")
+library("MALDIquantForeign")
+library("MALDIquantExamples")
+
+spectra <- import(getPathFiedler2009()["spectra"], verbose=FALSE)
+
+spectra.info <- read.table(getPathFiedler2009()["info"], sep=",", header=TRUE)
+
+spectra <- trim(spectra)
+
+set.seed(2019)
+
+spectra <- transformIntensity(spectra, method="sqrt")
+
+spectra <- smoothIntensity(spectra, method="SavitzkyGolay", halfWindowSize=20)
+
+spectra <- removeBaseline(spectra, method="SNIP", iterations=150)
+
+spectra <- calibrateIntensity(spectra, method="TIC")
+
+spectra <- alignSpectra(spectra)
+
+avgSpectra <- averageMassSpectra(spectra, labels=spectra.info$patientID)
+avgSpectra.info <- spectra.info[!duplicated(spectra.info$patientID), ]
+
+peaks <- detectPeaks(avgSpectra, SNR=2, halfWindowSize=20)
+
+peaks.binned <- binPeaks(peaks)
+
+peaks.binned.filtered <- filterPeaks(peaks.binned, minFrequency=c(0.5, 0.5), labels=avgSpectra.info$health, mergeWhitelists=TRUE)
+
+binnedPeaksMatrix <- intensityMatrix(peaks.binned.filtered, avgSpectra)
+rownames(binnedPeaksMatrix) <- avgSpectra.info$patientID
+
+binnedPeaksMatrix.conditions <- avgSpectra.info$health
---
+++ load-maldiquant-species.R
@@ -0,0 +1,41 @@
+library("MALDIquant")
+library("MALDIquantForeign")
+library("MALDIquantExamples")
+
+spectra <- import(getPathSpecies(), verbose=FALSE)
+
+spectra <- trim(spectra)
+
+set.seed(2019)
+
+spectra <- transformIntensity(spectra, method="sqrt")
+
+spectra <- smoothIntensity(spectra, method="SavitzkyGolay", halfWindowSize=10)
+
+spectra <- removeBaseline(spectra, method="SNIP", iterations=25)
+
+spectra <- calibrateIntensity(spectra, method="TIC")
+
+spectra <- alignSpectra(spectra)
+
+metaData(spectra[[1]])$spot
+
+spots <- sapply(spectra, function(x)metaData(x)$spot)
+species <- sapply(spectra, function(x)metaData(x)$sampleName)
+
+avgSpectra <- averageMassSpectra(spectra, labels=paste0(species, spots))
+
+peaks <- detectPeaks(avgSpectra, SNR=2, halfWindowSize=10)
+
+peaks.binned <- binPeaks(peaks)
+
+peaks.binned.filtered <- filterPeaks(peaks.binned, minFrequency=0.25)
+
+spots <- sapply(avgSpectra, function(x)metaData(x)$spot)
+species <- sapply(avgSpectra, function(x)metaData(x)$sampleName)
+species <- factor(species) 
+
+binnedPeaksMatrix <- intensityMatrix(peaks.binned.filtered, avgSpectra)
+rownames(binnedPeaksMatrix) <- paste(species, spots, sep=".")
+
+binnedPeaksMatrix.conditions <- species
