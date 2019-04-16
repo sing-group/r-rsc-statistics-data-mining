ecc4039c3dbb930e8d29710a00ce208e7357ffda
Hugo López-Fernández <hlfernandez@uvigo.es>
2019-04-16 11:34:11 +0200

    Extract functions to the peak-ranking-fold-change-functions.R file

---
+++ peak-ranking-fold-change-functions.R
@@ -0,0 +1,45 @@
+meanConditionSpectra <- function(conditions, peaksMatrix, rowsConditions) {
+    avgPeaks <- c()
+
+    for(condition in conditions) {
+        spectraIndexes <- which(rowsConditions == condition)
+        avgPeaks <- rbind(avgPeaks, apply(peaksMatrix[spectraIndexes,], 2, mean))
+    }
+
+    rownames(avgPeaks) <- conditions
+    
+    avgPeaks
+}
+
+compareConditions <- function(averagePeaksMatrix, zeroIncrease=0) {
+    avgPeaks <- averagePeaksMatrix
+    comparisons <- combn(rownames(avgPeaks), 2)
+    result <- matrix(nrow = ncol(avgPeaks), ncol = ncol(comparisons))
+    rownames(result) <- colnames(avgPeaks)
+    colnames(result) <- apply(comparisons,2, function(x) paste0(x[1], " / ", x[2]))
+
+    for(peak in colnames(avgPeaks)) {
+        for(cmpIndex in 1:ncol(comparisons)) {
+            a <- comparisons[1, cmpIndex]
+            b <- comparisons[2, cmpIndex]
+
+            avg.a <- avgPeaks[a, peak]
+            if(avg.a == 0) {
+                avg.a <- zeroIncrease
+            }
+            
+            avg.b <- avgPeaks[b, peak]
+            if(avg.b == 0) {
+                avg.b <- zeroIncrease
+            }
+
+            avg <- avg.a / avg.b
+            if(is.infinite(avg) | is.nan(avg)) {
+                avg <- NA
+            }
+            result[peak, cmpIndex] <- avg
+        }
+    }
+    
+    result
+}
--- peak-ranking-fold-change.R
+++ peak-ranking-fold-change.R
@@ -1,59 +1,14 @@
 source("load-cancer.R")
+source("peak-ranking-fold-change-functions.R")
 
 imagesDirectory <- "images/important-variables/"
 
 dir.create(imagesDirectory, recursive=TRUE, showWarnings=FALSE)
 
-meanConditionSpectra <- function(conditions, peaksMatrix, rowsConditions) {
-    avgPeaks <- c()
-
-    for(condition in conditions) {
-        spectraIndexes <- which(rowsConditions == condition)
-        avgPeaks <- rbind(avgPeaks, apply(peaksMatrix[spectraIndexes,], 2, mean))
-    }
-
-    rownames(avgPeaks) <- conditions
-    
-    avgPeaks
-}
-
 avgPeaks <- meanConditionSpectra(consensusData$datasetConditions, consensusBinnedPeaksMatrix, consensusData$spectraConditions)
 
 # log2(avgPeaks["HEALTHY", "2984.26172166667"] / avgPeaks["LYMPHOMA", "2984.26172166667"])
 # log2(avgPeaks["HEALTHY", "2984.26172166667"] / avgPeaks["MYELOMA", "2984.26172166667"])
-
-compareConditions <- function(averagePeaksMatrix, zeroIncrease=0) {
-    avgPeaks <- averagePeaksMatrix
-    comparisons <- combn(rownames(avgPeaks), 2)
-    result <- matrix(nrow = ncol(avgPeaks), ncol = ncol(comparisons))
-    rownames(result) <- colnames(avgPeaks)
-    colnames(result) <- apply(comparisons,2, function(x) paste0(x[1], " / ", x[2]))
-
-    for(peak in colnames(avgPeaks)) {
-        for(cmpIndex in 1:ncol(comparisons)) {
-            a <- comparisons[1, cmpIndex]
-            b <- comparisons[2, cmpIndex]
-
-            avg.a <- avgPeaks[a, peak]
-            if(avg.a == 0) {
-                avg.a <- zeroIncrease
-            }
-            
-            avg.b <- avgPeaks[b, peak]
-            if(avg.b == 0) {
-                avg.b <- zeroIncrease
-            }
-
-            avg <- avg.a / avg.b
-            if(is.infinite(avg) | is.nan(avg)) {
-                avg <- NA
-            }
-            result[peak, cmpIndex] <- avg
-        }
-    }
-    
-    result
-}
 
 comparison <- compareConditions(avgPeaks)
 
