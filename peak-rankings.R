807bf9dc40b6ef745c527ded68fa2802f0eff37b
Hugo López-Fernández <hlfernandez@uvigo.es>
2019-04-19 16:25:19 +0200

    Rename peak ranking files

--- peak-rankings.R
+++ peak-rankings.R
@@ -1,13 +1,48 @@
-source("load-cancer.R")
-source("distance-measures.R")
-
 imagesDirectory <- "images/important-variables/"
 
 dir.create(imagesDirectory, recursive=TRUE, showWarnings=FALSE)
 
 #	---------------------------------------------------------------------------
 #
-#	1. Ranking features using the sda.ranking function from the SDA package
+#	1 Fold-changes
+#
+#	---------------------------------------------------------------------------
+
+source("load-cancer.R")
+
+source("peak-rankings-functions.R")
+
+avgPeaks <- meanConditionSpectra(consensusData$datasetConditions, consensusBinnedPeaksMatrix, consensusData$spectraConditions)
+
+# log2(avgPeaks["HEALTHY", "2984.26172166667"] / avgPeaks["LYMPHOMA", "2984.26172166667"])
+# log2(avgPeaks["HEALTHY", "2984.26172166667"] / avgPeaks["MYELOMA", "2984.26172166667"])
+
+comparison <- compareConditions(avgPeaks)
+
+comparison.log2 <- log2(comparison)
+
+comparison.index <- 1
+
+data <- sort(comparison.log2[,comparison.index])
+data.plot <- data[!is.infinite(data) & abs(data) > 1.5]
+
+png(paste0(imagesDirectory, "peak-ranking-fold-changes.png"), width = 1200, height = 800)
+barplot(data.plot, horiz=TRUE, names=substr(names(data.plot), 1, 8), las=1, space=1.5, cex.names=0.8, xlim=c(min(data.plot)-1, max(data.plot)+1), main = colnames(comparison)[comparison.index])
+dev.off()
+
+#	---------------------------------------------------------------------------
+#
+#	2 t-scores
+#
+#	---------------------------------------------------------------------------
+
+source("load-cancer.R")
+
+source("distance-measures.R")
+
+#	---------------------------------------------------------------------------
+#
+#	2.1 Ranking features using the sda.ranking function from the SDA package
 #
 #		Homepage: http://strimmerlab.org/software/sda/
 #		CRAN: http://cran.r-project.org/package=sda
@@ -23,7 +58,7 @@ dev.off()
 
 #	---------------------------------------------------------------------------
 #
-#	2. Using the 'X' top peaks to perform a Hierarchical Clustering Analysis 
+#	2.2 Using the 'X' top peaks to perform a Hierarchical Clustering Analysis 
 #       (using Jaccard distance)
 #
 #	---------------------------------------------------------------------------
