46c8cf63d51abbf6d580957ade5ce51034172567
Hugo López-Fernández <hlfernandez@uvigo.es>
2019-04-23 17:31:13 +0200

    Use sum(withinss) instead of tot.withinss

--- kmeans-clustering.R
+++ kmeans-clustering.R
@@ -12,6 +12,7 @@ source("load-cancer.R")
 
 # Since k-means starts its centroids randomnly, we set a random seed in order
 # to reproduce the same results
+
 set.seed(2019)
 
 #	---------------------------------------------------------------------------
@@ -19,6 +20,7 @@ set.seed(2019)
 #	1.1 K-means using the replicates matrix (binnedPeaksMatrix)
 #
 #	---------------------------------------------------------------------------
+
 clustering.kmeans <- kmeans(binnedPeaksMatrix, centers=3)
 clustering.kmeans$cluster
 
@@ -27,6 +29,7 @@ clustering.kmeans$cluster
 #	1.2 K-means using the samples matrix (consensusBinnedPeaksMatrix)
 #
 #	---------------------------------------------------------------------------
+
 clustering.kmeans.consensus <- kmeans(consensusBinnedPeaksMatrix, centers=3)
 clustering.kmeans.consensus$cluster
 
@@ -35,6 +38,7 @@ clustering.kmeans.consensus$cluster
 #	1.3 K-means using the replicates matrix (binnedPeaksMatrix) and using presence/absence 
 #
 #	---------------------------------------------------------------------------
+
 presenceBinnedPeaksMatrix <- asPresenceMatrix(binnedPeaksMatrix)
 clustering.kmeans.presence <- kmeans(binnedPeaksMatrix, centers=3)
 clustering.kmeans.presence$cluster
@@ -44,6 +48,7 @@ clustering.kmeans.presence$cluster
 #	1.4 K-means using the samples matrix (consensusBinnedPeaksMatrix) and using presence/abscence 
 #
 #	---------------------------------------------------------------------------
+
 presenceConsensusBinnedPeaksMatrix <- asPresenceMatrix(consensusBinnedPeaksMatrix)
 clustering.kmeans.presence.consensus <- kmeans(consensusBinnedPeaksMatrix, centers=3)
 clustering.kmeans.presence.consensus$cluster
@@ -51,14 +56,16 @@ clustering.kmeans.presence.consensus$cluster
 
 #	---------------------------------------------------------------------------
 #
-#	1.5 Try different number of clusters and plot the tot.withinss
+#	1.5 Try different number of clusters and plot the total withinss (calculated
+#       with sum(withinss) or using tot.withinss
 #
 #	---------------------------------------------------------------------------
+
 clustering.kmeans.k_withinss <- vector()
 for (c in 1:10) {
   set.seed(2019)
   clust <- kmeans(binnedPeaksMatrix, centers=c)
-  clustering.kmeans.k_withinss[c] <- clust$tot.withinss
+  clustering.kmeans.k_withinss[c] <- sum(clust$withinss)
 }
 png(paste0(imagesDirectory, "clustering-kmeans-k-withinss.png"), width=640, height=480)
 plot(clustering.kmeans.k_withinss, type="b", xlab="Number of clusters", ylab="total within_SS", main="K-means quality at different k (replicates matrix)")
@@ -68,7 +75,7 @@ clustering.kmeans.k_withinss <- vector()
 for (c in 1:10) {
   set.seed(2019)
   clust <- kmeans(consensusBinnedPeaksMatrix, centers=c)
-  clustering.kmeans.k_withinss[c] <- clust$tot.withinss
+  clustering.kmeans.k_withinss[c] <- sum(clust$withinss)
 }
 png(paste0(imagesDirectory, "clustering-kmeans-samples-k-withinss.png"), width=640, height=480)
 plot(clustering.kmeans.k_withinss, type="b", xlab="Number of clusters", ylab="total within_SS", main="K-means quality at different k (samples matrix)")
@@ -79,7 +86,9 @@ dev.off()
 #	1.6 Get a consensus clustering with ConsensusClusterPlus
 #
 #	---------------------------------------------------------------------------
-library(ConsensusClusterPlus)
+
+library("ConsensusClusterPlus")
+
 pdf(paste0(imagesDirectory, "consensus-cluster-plus.pdf"))
 ccp <- ConsensusClusterPlus(t(binnedPeaksMatrix), maxK=3, distance="euclidean", clusterAlg="km", reps=50, seed=2019)
 dev.off()
