6eaf4e6f6ca9a56606794a933b41f723a9f6f500
Hugo López-Fernández <hlfernandez@uvigo.es>
2019-04-29 12:56:56 +0200

    Fix condition names

--- pca.R
+++ pca.R
@@ -24,20 +24,19 @@ summary(pca)
 pc95 <- min(which(cumsum(pca$sdev^2)/sum(pca$sdev^2) >= 0.95))
 binnedPeaksMatrix.pca.95 <- pca$x[,1:pc95]
 
-sample_conditions <- sapply(rownames(binnedPeaksMatrix), function(x) substr(x,1,1))
-
 png(paste0(imagesDirectory, "pca-replicates.png"), width=640, height=480)
-pca2d(pca, group=sample_conditions, components=c(1,2), legend="bottomright")
+pca2d(pca, group=data$spectraConditions, components=c(1,2), legend="bottomright")
 dev.off()
 
 # Uncomment the following line to show the 3D visualiation of the PCA
-# pca3d(pca, group=sample_conditions, components=c(1,2,3), legend="bottomright")
+# pca3d(pca, group=data$spectraConditions, components=c(1,2,3), legend="bottomright")
 
 #	---------------------------------------------------------------------------
 #
 #	1.2 Principal Components Analysis using the samples matrix (consensusBinnedPeaksMatrix)
 #
 #	---------------------------------------------------------------------------
+
 pca <- prcomp(consensusBinnedPeaksMatrix, scale=TRUE)
 summary(pca)
 
@@ -45,16 +44,14 @@ summary(pca)
 pc95 <- min(which(cumsum(pca$sdev^2)/sum(pca$sdev^2) >= 0.95))
 binnedPeaksMatrix.pca.95 <- pca$x[,1:pc95]
 
-sample_conditions <- sapply(rownames(consensusBinnedPeaksMatrix), function(x) substr(x,1,1))
-
 png(paste0(imagesDirectory, "pca-samples.png"), width=640, height=480)
-pca2d(pca, group=sample_conditions, components=c(1,2), legend="bottomright")
+pca2d(pca, group=data$samplesConditions, components=c(1,2), legend="bottomright")
 dev.off()
 
 #with biplot
 png(paste0(imagesDirectory, "pca-samples-biplot.png"), width=640, height=480)
-pca2d(pca, group=sample_conditions, components=c(1,2), legend="bottomright", biplot=TRUE)
+pca2d(pca, group=data$samplesConditions, components=c(1,2), legend="bottomright", biplot=TRUE)
 dev.off()
 
 # Uncomment the following line to show the 3D visualiation of the PCA
-# pca3d(pca, group=sample_conditions, components=c(1,2,3), legend="bottomright")
+# pca3d(pca, group=data$samplesConditions, components=c(1,2,3), legend="bottomright")
